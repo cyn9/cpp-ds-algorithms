182466d2981877fc26670e7e4eca41e42858f403
cyn9 <cihan.asci@tufts.edu>
2021-08-09 15:49:41 +0300

    Swap two integers using XOR.

---
+++ 00-Basics/swap-xor.cpp
@@ -0,0 +1,21 @@
+// Swap two integers using XOR.
+#include <iostream>
+
+int main() {
+    int a {0};
+    int b {0};
+    std::cout << "Enter two numbers: ";
+    std::cin >> a >> b;
+    
+    std::cout << "Before swapping: ";
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    
+    a = a^b;
+    b = a^b;
+    a = a^b;
+    
+    std::cout << "After swapping: ";
+    std::cout << "a = " << a << ", b = " << b << std::endl; 
+
+    return 0;
+}
