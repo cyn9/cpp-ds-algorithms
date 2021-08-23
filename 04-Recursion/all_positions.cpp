50a04addaac21417e58684d363c1592b067047c3
cyn9 <cihan.asci@tufts.edu>
2021-08-24 00:09:25 +0300

    Print all positions of an element in an array.

---
+++ 04-Recursion/all_positions.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+void print(const int [], const std::size_t &);
+void getIndex(const int [], const std::size_t &, int &, int);
+
+int main() {
+    int index {0};
+    int value {0};
+    
+    std::size_t size {0};
+
+    std::cout << "Enter the number of elements >> ";
+    std::cin >> size;
+
+    int arr[size];
+
+    for (auto i = 0; i < size; ++i) {
+        std::cout << "Enter element " << (i+1) << " >> ";
+        std::cin >> arr[i];
+    }
+
+    print(arr, size);
+
+    std::cout << "Enter a value >> ";
+    std::cin >> value;
+
+    getIndex(arr, size, value, index);
+
+    return 0;
+}
+
+void print(const int arr[], const std::size_t &size) {
+    std::cout << "Array elements >> ";
+
+    for (auto i = 0; i < size; ++i)
+        std::cout << arr[i] << " ";
+
+    std::cout << '\n';
+}
+
+void getIndex(const int arr[], const std::size_t &N, int &value, int index) {
+    if (N == index) { std::cout << '\n'; return; }
+
+    if (arr[index] == value)
+        std::cout << index << " ";
+
+    return getIndex(arr, N, value, index+1);
+}
