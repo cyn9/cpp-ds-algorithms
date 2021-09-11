303b3b4ca0adc2d9aa78852ceacf826135ca8d38
cyn9 <cihan.asci@tufts.edu>
2021-09-11 18:21:08 -0400

    Added the reverse direction when creating linked list

--- 05-Linked-List/input_test.cpp
+++ 05-Linked-List/input_test.cpp
@@ -13,12 +13,21 @@ template<typename T>
 void print(Node<T> *);
 
 template<typename T>
-Node<T>* create();
+Node<T>* createFwd();
+
+template<typename T>
+Node<T>* createRev();
 
 int main() {
-    Node<int>* n = create<int>(); // Head of node n
+    // Node<int>* n1 = createFwd<int>(); // Head of node n1
+    Node<int>* n2 = createRev<int>(); // Head of node n2
 
-    print(n);
+    // std::cout << "n1 >> ";
+    // print(n1);
+
+    std::cout << "n2 >> ";
+    print(n2);
+
     return 0;
 }
 
@@ -38,8 +47,9 @@ void print(Node<T>* nodePtr) {
 }
 
 // Take input from the user:
+// Create a linked list in forward direction:
 template<typename T>
-Node<T>* create() {
+Node<T>* createFwd() {
     T data;
     std::cout << "Enter data >> ";
     std::cin >> data;
@@ -59,6 +69,36 @@ Node<T>* create() {
             // If there are elements present in the LL:
             tail->next = n; // Connect the last to the new element.
             tail = n; // Update the tail node.
+        }
+
+        std::cout << "Enter data >> ";
+        std::cin >> data;
+    }
+
+    return head;
+}
+
+// Take input from the user:
+// Create a linked list in reverse direction:
+template<typename T>
+Node<T>* createRev() {
+    T data;
+    std::cout << "Enter data >> ";
+    std::cin >> data;
+
+    Node<T>* head = nullptr; // LL is initially empty.
+
+    while (data != -1) {
+        Node<T>* n = new Node<T>(data);
+
+        // Check if n is the first element:
+        if (head == nullptr) {
+            head = n;
+        }
+        else {
+            // If there are elements present in the LL:
+            n->next = head; // Connect the first to the new element.
+            head = n; // Update the head node.
         }
 
         std::cout << "Enter data >> ";
