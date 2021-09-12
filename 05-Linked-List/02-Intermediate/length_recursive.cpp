4ef16b58b2119810cc0fa6baa019d9df467e2673
cyn9 <cihan.asci@tufts.edu>
2021-09-12 15:57:13 -0400

    Length of a linked list using recursion.

--- 05-Linked-List/02-Intermediate/length_recursive.cpp
+++ 05-Linked-List/02-Intermediate/length_recursive.cpp
@@ -1,6 +1,58 @@
 #include <iostream>
 
+template<typename T>
+struct Node {
+    T data;
+    Node<T>* next;
+
+    Node() = default;
+    Node(T data) : data{data}, next{nullptr} {}
+};
+
+// Take input from the user:
+// Create a linked list in forward direction:
+template<typename T>
+Node<T>* create() {
+    T data;
+    std::cout << "Enter data >> ";
+    std::cin >> data;
+
+    Node<T>* head = nullptr; // LL is initially empty.
+    Node<T>* tail = nullptr; // No elements present in the LL.
+
+    while (data != -1) {
+        Node<T>* n = new Node<T>(data);
+
+        // Check if n is the first element:
+        if (head == nullptr) {
+            head = n;
+            tail = n;
+        }
+        else {
+            // If there are elements present in the LL:
+            tail->next = n; // Connect the last to the new element.
+            tail = n; // Update the tail node.
+        }
+
+        std::cout << "Enter data >> ";
+        std::cin >> data;
+    }
+
+    return head;
+}
+
+// T = O(n), S = O(n)
+template<typename T>
+std::size_t length(const Node<T>* head) {
+    if (head == nullptr) { return 0; }
+
+    return static_cast<std::size_t> (1 + length(head->next));
+}
+
 int main() {
+    Node<int>* n = create<int>();
+
+    std::cout << "Length: " << length(n) << '\n';
 
     return 0;
 }
