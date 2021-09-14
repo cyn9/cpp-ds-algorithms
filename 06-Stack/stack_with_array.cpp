a502abae81e709ed1a8dfd7b79a6f760c9cc92ec
cyn9 <cihan.asci@tufts.edu>
2021-09-13 23:25:52 -0400

    Simple stack implementation using a dynamic array.

--- 06-Stack/stack_with_array.cpp
+++ 06-Stack/stack_with_array.cpp
@@ -15,10 +15,35 @@ class Stack {
         ~Stack() { delete[] m_data; }
 
     public:
-        void push(T value) { m_data[m_currentIndex++] = value; }
+        void push(T value) {
+            if (m_currentIndex == m_size)
+                std::cout << "ERROR: Cannot push element " << value << ". Stack overflow.\n";
+            else
+                m_data[m_currentIndex++] = value;
+        }
         
-        T pop()                       { return this->m_data[m_currentIndex--]; }
-        T top()                 const { return this->m_data[m_currentIndex-1]; }
+        T pop() {
+            if (this->is_empty()) {
+                std::cout << "ERROR: Cannot perform pop. Stack underflow.\n";
+                std::cout << "Returning -1 instead.\n";
+                return -1;
+            }
+            else {
+                return this->m_data[m_currentIndex--]; 
+            }
+        }
+        
+        T top() const {
+            if (this->is_empty()) {
+                std::cout << "Stack is empty. No top value in the stack.\n";
+                std::cout << "Returning -1 instead.\n";
+                return -1;
+            }
+            else {
+                return this->m_data[m_currentIndex-1]; 
+            }
+        }
+
         std::size_t size()      const { return this->m_currentIndex; }
         std::size_t capacity()  const { return this->m_size; }
         bool is_empty()         const { return this->m_currentIndex == 0; }
@@ -46,6 +71,20 @@ int main() {
 
     std::cout << "Stack size is " << s.size() << '\n';
     std::cout << "Stack capacity is " << s.capacity() << '\n';
+
+    std::cout << "\nTesting stack underflow: \n";
+    for (std::size_t i = 0; i < 4; ++i) {
+        std::cout << "Popping element: " << s.top() << '\n';
+        s.pop();
+    }
+    s.print();
+
+    std::cout << "\nTesting stack overflow: \n";
+    for (auto i = 0; i < 11; ++i) {
+        std::cout << "Pushing element: " << i << '\n';
+        s.push(i);
+    }
+    s.print();
 
     return 0;
 }
